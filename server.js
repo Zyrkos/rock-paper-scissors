92999fbeaceb4d79fe62e63cea368fdd5b6fd73e
Zyrkos <pedrocastro1995@hotmail.com>
2022-06-22 16:42:39 +0100

    server.js

--- server.js
+++ server.js
@@ -4,28 +4,33 @@ function game () {
     playRound();
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
     const playerSelection = playerChoice();
-    const computerSelection = computerPlay();
-
-
+    const computerSelection = computerChoice();
 }
 
-function computerPlay() {
+function playerChoice() {
+    let input = prompt("Type Rock, Paper or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper or Scissors");
+        //This makes it so that when you press cancel, nothing goes wrong with the code
+    }
+    input = input.toLocaleLowerCase();
+    console.log(input);
+    
+    
+}
+
+function computerChoice() {
     return choices[Math.floor(Math.random()*3)];
 //This function randomly selects an item from the "choices" array
-//You can test if the function is functioning by using: console.log(computerPlay(choices));
-
+//You can test if the function is working by using: console.log(computerChoice(choices));
+//In Math.random we multiply by 3 since that is the number of strings inside the 'choices' array.
 }
 
-function playerChoice(){
-    let input = prompt('Type Rock, Paper or Scissors');
-    
+let input = playerChoice();
 
 
-}
-
-game();
 
 
 
