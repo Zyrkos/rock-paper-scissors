197515c1db58e3711c9195a7af43c06f91976f9b
Zyrkos <pedrocastro1995@hotmail.com>
2022-06-22 16:18:31 +0100

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const choices = ['Rock', 'Paper', 'Scissors'];
 
-function computerPlay() {
-    return choices[Math.floor(Math.random()*3)];
+function game () {
+    playRound();
+}
+
+function playRound(playerSelection, computerSelection) {
+    const playerSelection = playerChoice();
+    const computerSelection = computerPlay();
+
 
 }
+
+function computerPlay() {
+    return choices[Math.floor(Math.random()*3)];
 //This function randomly selects an item from the "choices" array
+//You can test if the function is functioning by using: console.log(computerPlay(choices));
+
+}
+
+function playerChoice(){
+    let input = prompt('Type Rock, Paper or Scissors');
+    
 
 
-console.log(computerPlay(choices));
+}
+
+game();
+
+
+
