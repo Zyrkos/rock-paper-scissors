c387341b87e545e79bfb29b764ec511e549a3d9a
Zyrkos <pedrocastro1995@hotmail.com>
2022-06-23 13:18:25 +0100

    server.js

--- server.js
+++ server.js
@@ -4,17 +4,17 @@
 
 
 function computerPlay() {
-    const choice = ["Rock", "Paper", "Scissors"];
+    const choice = ["Rock", "Paper", "Scissors"]; //array
     const randomChoice = Math.floor(Math.random()*3);
     //This function makes the computer randomly select an item from a given array. 
     //In this case, we create an array with the options rock, paper and scissors, and then use
     //Math.random(*3, since that is the number of items in the array), which gets any number
     //between 0 and 1 (but never 1), Math.floor then approximates it.
-    console.log(choice[randomChoice])
+    const computerChoice = choice[randomChoice];
+    return computerChoice;
 }
 
-computerPlay();
-
+console.log(computerPlay());
 
 
 
